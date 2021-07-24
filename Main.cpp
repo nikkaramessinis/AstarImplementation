82aa6c7fc0f41c4a6ba16429bf179342b88bf441
Nick Karamesinis <nikaramessinis@gmail.com>
2021-07-24 23:03:25 +0100

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,23 @@
 #include <SDL.h>
 #include <iostream>
 #include "node.h"
-#include <list>
+#include <queue>          // std::priority_queue
 #undef main
 
+const int numOfTiles = 15;
 
-int sourceX =-1, sourceY=-1;
-int destX=-1, destY=-1;
+int sourceX = -1, sourceY = -1;
+int destX = -1, destY = -1;
 bool ctrlPressed = false;
 int tmpObstacleX = -1, tmpObstacleY = -1;
-node *PathMap[10][10];
-SDL_Rect *rects[10][10];
-node *NodeEnd,* NodeStart;
+node* PathMap[15][15];
+SDL_Rect* rects[15][15];
+node* NodeEnd, * NodeStart;
 bool drawPath = false;
 
 void init() {
-	for (size_t x = 0; x < 10; x++) {
-		for (size_t y = 0; y < 10; y++)
+	for (size_t x = 0; x < numOfTiles; x++) {
+		for (size_t y = 0; y < numOfTiles; y++)
 		{
 			PathMap[x][y] = new node(x, y);
 			rects[x][y] = new SDL_Rect();
@@ -35,7 +36,7 @@ void findNeighbors(int x, int y) {
 			aFromTile->vecNeighbors.push_front(PathMap[x][y - 1]);
 		}
 	}
-	if (y < 9) {
+	if (y < 14) {
 		if (PathMap[x][y + 1]->visited == false && PathMap[x][y + 1]->obstacle == false) {
 			aFromTile->vecNeighbors.push_front(PathMap[x][y + 1]);
 		}
@@ -46,7 +47,7 @@ void findNeighbors(int x, int y) {
 			aFromTile->vecNeighbors.push_front(PathMap[x - 1][y]);
 		}
 	}
-	if (x < 9) {
+	if (x < 14) {
 		if (PathMap[x + 1][y]->visited == false && PathMap[x + 1][y]->obstacle == false) {
 			aFromTile->vecNeighbors.push_front(PathMap[x + 1][y]);
 		}
@@ -54,8 +55,8 @@ void findNeighbors(int x, int y) {
 	return;
 }
 void findallNeighbors() {
-	for (size_t x = 0; x < 10; x++) {
-		for (size_t y = 0; y < 10; y++)
+	for (size_t x = 0; x < numOfTiles; x++) {
+		for (size_t y = 0; y < numOfTiles; y++)
 		{
 			findNeighbors(x, y);
 		}
@@ -65,7 +66,7 @@ void findallNeighbors() {
 
 void render(SDL_Renderer* renderer) {
 	if (drawPath == true) {
-		node* first= PathMap[NodeEnd->x][NodeEnd->y];
+		node* first = PathMap[NodeEnd->x][NodeEnd->y];
 
 		node* p = PathMap[NodeEnd->x][NodeEnd->y];
 
@@ -80,22 +81,24 @@ void render(SDL_Renderer* renderer) {
 	}
 
 
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
-			SDL_Rect *rect = rects[i][j];
-			rect->h = 50;
-			rect->w = 50;
-			rect->x = 60 * i;
-			rect->y = 60 * j;
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 15; j++) {
+			SDL_Rect* rect = rects[i][j];
+			rect->h = 30;
+			rect->w = 30;
+			rect->x = 40 * i;
+			rect->y = 40 * j;
 			if (PathMap[i][j]->marked == true) {
 				SDL_SetRenderDrawColor(renderer, 211, 211, 211, 255);
 				SDL_RenderDrawRect(renderer, rect);
 				SDL_RenderFillRect(renderer, rect);
-			}else if (i == sourceX && j == sourceY || i == destX && j == destY) {
+			}
+			else if (i == sourceX && j == sourceY || i == destX && j == destY) {
 				SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 				SDL_RenderDrawRect(renderer, rect);
 				SDL_RenderFillRect(renderer, rect);
-			}else if(PathMap[i][j]->obstacle==true){
+			}
+			else if (PathMap[i][j]->obstacle == true) {
 				SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 				SDL_RenderDrawRect(renderer, rect);
 				SDL_RenderFillRect(renderer, rect);
@@ -107,7 +110,7 @@ void render(SDL_Renderer* renderer) {
 			}
 		}
 	}
-	
+
 	SDL_RenderPresent(renderer);
 
 }
@@ -116,8 +119,8 @@ void render(SDL_Renderer* renderer) {
 
 void SolveAstar(node* NodeStart, node* NodeEnd)
 {
-	for (int x = 0; x < 10; x++) {
-		for (int y = 0; y < 10; y++) {
+	for (int x = 0; x < numOfTiles; x++) {
+		for (int y = 0; y < numOfTiles; y++) {
 			PathMap[x][y]->visited = false;
 			PathMap[x][y]->fGlobalGoal = FLT_MAX;
 			PathMap[x][y]->fLocalGoal = FLT_MAX;
@@ -126,46 +129,49 @@ void SolveAstar(node* NodeStart, node* NodeEnd)
 
 		}
 	}
-	auto distance = [](node * a, node *b) {
-		return sqrtf(static_cast<float>((((a->x) - (b->x))*((a->x) - (b->x)) + ((a->y) - (b->y))*((a->y) - (b->y)))));
-	};
-	auto heuristic = [distance](node* a, node*b) {
-		return distance(a, b);
+	auto distance = [](node* a, node* b) {
+		return sqrtf(static_cast<float>((((a->x) - (b->x)) * ((a->x) - (b->x)) + ((a->y) - (b->y)) * ((a->y) - (b->y)))));
 	};
 	node* nodeCurrent = NodeStart;
 	NodeStart->fLocalGoal = 0.0f;
-	NodeStart->fGlobalGoal = heuristic(NodeStart, NodeEnd);
+	NodeStart->fGlobalGoal = distance(NodeStart, NodeEnd);
 
+	struct CustomCompare
+	{
+		bool operator()(const node* lhs, const node* rhs)
+		{
+			return lhs->fGlobalGoal < rhs->fGlobalGoal;;
+		}
+	};
+	std::priority_queue<node* , std::vector<node*>, CustomCompare> queueOpenNodes;
 
-	list<node *> listOpenNodes;
-	listOpenNodes.push_back(NodeStart);
+	queueOpenNodes.push(NodeStart);
 
 	//stop searching when finding a path
-	while (!listOpenNodes.empty()/* && nodeCurrent != NodeEnd*/) {
+	while (!queueOpenNodes.empty()/* && nodeCurrent != NodeEnd*/) {
 		//sort list into ascending order of global goals
-		listOpenNodes.sort([](const node* lhs, const node* rhs) {return lhs->fGlobalGoal < rhs->fGlobalGoal; });
 		//if the list is already visited pop
-		while (!listOpenNodes.empty() && listOpenNodes.front()->visited)
-			listOpenNodes.pop_front();
+		while (!queueOpenNodes.empty() && queueOpenNodes.top()->visited)
+			queueOpenNodes.pop();
 		//but this way we can end up with an empty list 
-		if (listOpenNodes.empty())
+		if (queueOpenNodes.empty())
 			break;
 
 		//so the first one is the possibly the best candidate to the shortest path
-		nodeCurrent = listOpenNodes.front();
+		nodeCurrent = queueOpenNodes.top();
 		nodeCurrent->visited = true;
 		//check neighbors of current node
 		for (auto nodeNeigbor : nodeCurrent->vecNeighbors) {
 			//and push_back them unless they're visited or an wall
 			if (!nodeNeigbor->visited && nodeNeigbor->obstacle == 0)
-				listOpenNodes.push_back(nodeNeigbor);
+				queueOpenNodes.push(nodeNeigbor);
 			//calculate neighbors local goals
 			float fPossiblyLowerGoal = nodeCurrent->fLocalGoal + distance(nodeCurrent, nodeNeigbor);
 			if (fPossiblyLowerGoal < nodeNeigbor->fLocalGoal) {
 				nodeNeigbor->parent = nodeCurrent;
 				nodeNeigbor->fLocalGoal = fPossiblyLowerGoal;
 				//and calculate global goal
-				nodeNeigbor->fGlobalGoal = nodeNeigbor->fLocalGoal + heuristic(nodeNeigbor, NodeEnd);
+				nodeNeigbor->fGlobalGoal = nodeNeigbor->fLocalGoal + distance(nodeNeigbor, NodeEnd);
 
 			}
 
@@ -181,8 +187,8 @@ void mousePress(SDL_MouseButtonEvent& b) {
 	if (b.button == SDL_BUTTON_LEFT && ctrlPressed == true) {
 		SDL_GetMouseState(&tmpObstacleX, &tmpObstacleY);
 		if (tmpObstacleX != -1 || tmpObstacleY != -1) {
-			tmpObstacleX = tmpObstacleX / 60;
-			tmpObstacleY = tmpObstacleY / 60;
+			tmpObstacleX = tmpObstacleX / 40;
+			tmpObstacleY = tmpObstacleY / 40;
 			PathMap[tmpObstacleX][tmpObstacleY]->obstacle = true;
 			if (NodeStart != nullptr && NodeEnd != nullptr) {
 				SolveAstar(NodeStart, NodeEnd);
@@ -193,19 +199,20 @@ void mousePress(SDL_MouseButtonEvent& b) {
 	else if (b.button == SDL_BUTTON_LEFT) {
 		SDL_GetMouseState(&sourceX, &sourceY);
 		if (sourceX != 0 || sourceY != 0) {
-			sourceX = sourceX / 60;
-			sourceY = sourceY / 60;
+			sourceX = sourceX / 40;
+			sourceY = sourceY / 40;
 			NodeStart = PathMap[sourceX][sourceY];
 			if (NodeStart != nullptr && NodeEnd != nullptr) {
 				SolveAstar(NodeStart, NodeEnd);
 				drawPath = true;
 			}
 		}
-	}else if(b.button == SDL_BUTTON_RIGHT) {
+	}
+	else if (b.button == SDL_BUTTON_RIGHT) {
 		SDL_GetMouseState(&destX, &destY);
 		if (destX != 0 || destY != 0) {
-			destX = destX / 60;
-			destY = destY / 60;
+			destX = destX / 40;
+			destY = destY / 40;
 			NodeEnd = PathMap[destX][destY];
 			if (NodeStart != nullptr && NodeEnd != nullptr) {
 				SolveAstar(NodeStart, NodeEnd);
@@ -236,20 +243,20 @@ void handleEvents() {
 int main() {
 	bool isRunning = true;
 	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window * window = SDL_CreateWindow("A*", SDL_WINDOWPOS_CENTERED,  SDL_WINDOWPOS_CENTERED, 600, 600,SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("A*", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
 	init();
 	NodeStart = nullptr;
 	NodeEnd = nullptr;
-	
+
 	findallNeighbors();
 	while (isRunning) {
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
 		handleEvents();
 		render(renderer);
-		
+
 	}
 	return 0;
 }
